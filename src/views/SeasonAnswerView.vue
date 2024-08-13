d7b39df8267761115f5d7c77cb9f5576d8c24d21
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2024-08-13 11:34:01 -0600

    add beginning text and structure

---
+++ src/views/SeasonAnswerView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>NO</p>
+    </div>
+    <div>
+        
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+.about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+}
+}
+</style>
